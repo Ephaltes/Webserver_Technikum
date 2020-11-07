44a115991722b515a74d3930cb385c79f32d5285
Ngoc Lam Nguyen <if19b072@technikum-wien.at>
2020-11-07 12:53:30 +0100

    added Message Interface and MessageModel

---
+++ WebServer/Model/IMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebServer.Model
+{
+    class Message
+    {
+        public int id;
+        public string message;
+    }
+    interface IMessageController
+    {
+        public List<Message> GetMessages();
+        public Message GetMessage(int id);
+
+        public int Add(string message);
+        public bool Delete(int id);
+        public bool Update(int id, string message);
+    }
+}
---
+++ WebServer/Model/Message.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Serilog.Events;
+
+namespace WebServer.Model
+{
+    class MessageController : IMessageController
+    {
+        private readonly List<Message> messageList;
+        private int lastid;
+
+        public MessageController()
+        {
+            messageList = new List<Message>();
+        }
+
+        public List<Message> GetMessages()
+        {
+            return messageList;
+        }
+
+        public Message GetMessage(int id)
+        {
+            return messageList.FirstOrDefault(a => a.id == id);
+        }
+
+        public int Add(string message)
+        {
+            lastid++;
+            Message temp = new Message
+            {
+                message = message,
+                id = lastid
+            };
+
+            messageList.Add(temp);
+            return temp.id;
+        }
+
+        public bool Delete(int id)
+        {
+            Message temp = messageList.FirstOrDefault(a => a.id == id);
+            if (temp == null)
+                return false;
+
+            messageList.Remove(temp);
+            return true;
+        }
+
+        public bool Update(int id, string message)
+        {
+            Message mes = messageList.FirstOrDefault(a => a.id == id);
+            if (mes == null)
+                return false;
+            mes.message = message;
+            return true;
+        }
+
+    }
+}
